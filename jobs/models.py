2a125e7dd1cff103b1a2ec90b58cde2b572a3328
Parag <parag.raut@outlook.com>
2018-09-30 18:19:38 +0200

    changed database to postgresql

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+    image = models.ImageField(upload_to="images/")
+    summary = models.CharField(blank=False, max_length=200)
+
+
+    class Meta:
+        verbose_name = 'Job'
+        verbose_name_plural = 'Jobs'
+
+    def __unicode__(self):
+        pass
