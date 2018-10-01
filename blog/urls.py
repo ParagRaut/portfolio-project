c377502dc891e7f9b48c09759cca6d27333b74c3
Parag <parag.raut@outlook.com>
2018-10-01 13:24:24 +0200

    Added blog

--- blog/urls.py
+++ blog/urls.py
@@ -13,15 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
-from jobs.views import home_page
+from .views import all_blogs
 
 urlpatterns = [
-    path('', home_page, name='home_page'),
-    path('blog/', include('blog.urls')),
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', all_blogs, name='all_blogs'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
+def all_blogs(request):
+    Blogs = Blog.objects.all()
+    context = {'Blogs':Blogs}
+    return render(request, 'blogs/all_blogs.html', context)
